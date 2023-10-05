b52fb6aa15a3f1047daa0d6cc163db0d2c3b53ee
Phong Pham <ducphong2713@gmail.com>
2023-10-05 20:31:25 +0700

    feat/signinANDsignup

--- DA_Frontend/src/pages/UserSide/SigninAndSignup/SigninPage.tsx
+++ DA_Frontend/src/pages/UserSide/SigninAndSignup/SigninPage.tsx
@@ -1,92 +1,104 @@
-import React from 'react';
-import { Button, Checkbox, Form, Input } from 'antd';
-
+import React from "react";
+import { Button, Checkbox, Form, Input } from "antd";
+import { Link } from "react-router-dom";
 
 const SigninPage = () => {
-  
-
-  
   type FieldType = {
     username?: string;
     password?: string;
+    role?: string;
     remember?: string;
   };
   const onFinish = (values: any) => {
-    console.log('Success:', values);
+    console.log("Success:", values);
   };
-  
+
   const onFinishFailed = (errorInfo: any) => {
-    console.log('Failed:', errorInfo);
+    console.log("Failed:", errorInfo);
   };
 
   return (
     <div>
-        <div className="container h-100; vh-100">
-          <div className="row d-flex justify-content-center align-items-center h-100">
-            <div className="col-lg-12 col-xl-11">
-              <div className="card text-black" style={{ borderRadius: '25px' }}>
-                <div className="card-body p-md-5">
-                  <div className="row justify-content-center">
-                    <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
-                    <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4"> Đăng nhập</p>
-    
-                      <div>
+      <div className="container h-100; vh-100">
+        <div className="row d-flex justify-content-center align-items-center h-100">
+          <div className="col-lg-12 col-xl-11">
+            <div className="card text-black" style={{ borderRadius: "25px" }}>
+              <div className="card-body p-md-5">
+                <div className="row justify-content-center">
+                  <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+                    <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">
+                      {" "}
+                      Đăng nhập
+                    </p>
+
+                    <div>
                       <Form
-    name="basic"
-    labelCol={{ span: 8 }}
-    wrapperCol={{ span: 16 }}
-    style={{ maxWidth: 600 }}
-    initialValues={{ remember: true }}
-    onFinish={onFinish}
-    onFinishFailed={onFinishFailed}
-    autoComplete="off"
-  >
-    <Form.Item<FieldType>
-      label="Username"
-      name="username"
-      rules={[{ required: true, message: 'Please input your username!' }]}
-    >
-      <Input  />
-    </Form.Item>
+                        name="basic"
+                        labelCol={{ span: 8 }}
+                        wrapperCol={{ span: 16 }}
+                        style={{ maxWidth: 600 }}
+                        initialValues={{ remember: true }}
+                        onFinish={onFinish}
+                        onFinishFailed={onFinishFailed}
+                        autoComplete="off"
+                      >
+                        <Form.Item<FieldType>
+                          label="Tài khoản"
+                          name="username"
+                          rules={[
+                            {
+                              required: true,
+                              message: "Vui lòng nhập tài khoản",
+                            },
+                          ]}
+                        >
+                          <Input />
+                        </Form.Item>
 
-    <Form.Item<FieldType>
-      label="Password"
-      name="password"
-      rules={[{ required: true, message: 'Please input your password!' }]}
-    >
-      <Input.Password />
-    </Form.Item>
+                        <Form.Item<FieldType>
+                          label="Mật khẩu"
+                          name="password"
+                          rules={[
+                            {
+                              required: true,
+                              message: "Vui lòng nhập mật khẩu",
+                            },
+                          ]}
+                        >
+                          <Input.Password />
+                        </Form.Item>
 
-    <Form.Item<FieldType>
-      name="remember"
-      valuePropName="checked"
-      wrapperCol={{ offset: 8, span: 16 }}
-    >
-      <Checkbox>Remember me</Checkbox>
-    </Form.Item>
-
-    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-      <Button type="primary" htmlType="submit">
-        Submit
-      </Button>
-    </Form.Item>
-  </Form>
-                      </div>
+                        <Form.Item<FieldType>
+                          name="remember"
+                          valuePropName="checked"
+                          wrapperCol={{ offset: 8, span: 16 }}
+                        >
+                          <Checkbox>Remember me</Checkbox>
+                        </Form.Item>
+                        
+                        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                          <Button type="primary" htmlType="submit">
+                            Đăng nhập
+                          </Button>
+                        </Form.Item>
+                      </Form>
+                      <span style={{ paddingLeft: '50px' }}>Bạn chưa có tài khoản? <Link style={{ textDecoration: 'none' }} to="/signup" >Đăng ký</Link></span>
+                      
                     </div>
-                    <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
-                      <img
-                        src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
-                        className="img-fluid"
-                        alt="Sample image"
-                      />
-                    </div>
+                  </div>
+                  <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+                    <img
+                      src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
+                      className="img-fluid"
+                      alt="Sample image"
+                    />
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      
+      </div>
     </div>
   );
 };
--- DA_Frontend/src/pages/UserSide/SigninAndSignup/SignupPage.tsx
+++ DA_Frontend/src/pages/UserSide/SigninAndSignup/SignupPage.tsx
@@ -1,67 +1,113 @@
-import React from 'react'
+import React from "react";
+import { Button, Checkbox, Form, Input } from "antd";
+import { Link } from "react-router-dom";
 
 const SignupPage = () => {
+  type FieldType = {
+    username?: string;
+    password?: string;
+    password2?: string;
+    email?: string;
+    remember?: string;
+  };
+  const onFinish = (values: any) => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log("Failed:", errorInfo);
+  };
+
   return (
     <div>
-       
-    <section className="vh-50" >
-      <div className="container h-50">
+      <div className="container  h-100; vh-100">
         <div className="row d-flex justify-content-center align-items-center h-100">
           <div className="col-lg-12 col-xl-11">
             <div className="card text-black" style={{ borderRadius: "25px" }}>
               <div className="card-body p-md-4">
                 <div className="row justify-content-center">
                   <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+                    <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">
+                      Đăng ký
+                    </p>
 
-                    <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</p>
+                    <div>
+                      <Form
+                        name="basic"
+                        labelCol={{ span: 8 }}
+                        wrapperCol={{ span: 16 }}
+                        style={{ maxWidth: 600 }}
+                        initialValues={{ remember: true }}
+                        onFinish={onFinish}
+                        onFinishFailed={onFinishFailed}
+                        autoComplete="off"
+                      >
+                        <Form.Item<FieldType>
+                          label="Tên đăng nhập"
+                          name="username"
+                          rules={[
+                            {
+                              required: true,
+                              message: "Vui lòng không bỏ trống tên đăng nhập",
+                            },
+                          ]}
+                        >
+                          <Input />
+                        </Form.Item>
+                        <Form.Item<FieldType>
+                          label="Email"
+                          name="email"
+                          rules={[
+                            {
+                              required: true,
+                              type: "email",
+                              message: "Vui lòng không bỏ trống email",
+                            },
+                          ]}
+                        >
+                          <Input />
+                        </Form.Item>
 
-                    <form className="mx-1 mx-md-4">
+                        <Form.Item<FieldType>
+                          label="Mật khẩu"
+                          name="password"
+                          rules={[
+                            {
+                              required: true,
+                              message: "Vui lòng không bỏ trống mật khẩu",
+                            },
+                          ]}
+                        >
+                          <Input.Password />
+                        </Form.Item>
+                        <Form.Item<FieldType>
+                          label="Nhập lại mật khẩu"
+                          name="password2"
+                          rules={[
+                            {
+                              required: true,
+                              message: "Vui lòng nhập lại mật khẩu ",
+                            },
+                          ]}
+                        >
+                          <Input.Password />
+                        </Form.Item>
 
-                      <div className="d-flex flex-row align-items-center mb-4">
-                        <i className="fas fa-user fa-lg me-3 fa-fw"></i>
-                        <div className="form-outline flex-fill mb-0">
-                          <input type="text" id="form3Example1c" className="form-control" />
-                          <label className="form-label" htmlFor="form3Example1c">Your Name</label>
-                        </div>
-                      </div>
-
-                      <div className="d-flex flex-row align-items-center mb-4">
-                        <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
-                        <div className="form-outline flex-fill mb-0">
-                          <input type="email" id="form3Example3c" className="form-control" />
-                          <label className="form-label" htmlFor="form3Example3c">Your Email</label>
-                        </div>
-                      </div>
-
-                      <div className="d-flex flex-row align-items-center mb-4">
-                        <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
-                        <div className="form-outline flex-fill mb-0">
-                          <input type="password" id="form3Example4c" className="form-control" />
-                          <label className="form-label" htmlFor="form3Example4c">Password</label>
-                        </div>
-                      </div>
-
-                      <div className="d-flex flex-row align-items-center mb-4">
-                        <i className="fas fa-key fa-lg me-3 fa-fw"></i>
-                        <div className="form-outline flex-fill mb-0">
-                          <input type="password" id="form3Example4cd" className="form-control" />
-                          <label className="form-label" htmlFor="form3Example4cd">Repeat your password</label>
-                        </div>
-                      </div>
-
-
-                      <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                        <button type="button" className="btn btn-primary btn-lg">Đăng ký</button>
-                      </div>
-
-                    </form>
-
+                        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                          <Button type="primary" htmlType="submit">
+                            Đăng ký
+                          </Button>
+                        </Form.Item>
+                      </Form>
+                      <span style={{ paddingLeft: '100px' }}>Bạn đã có tài khoản? <Link style={{ textDecoration: 'none' }} to="/signin" >Đăng nhập</Link></span>
+                    </div>
                   </div>
                   <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
-
-                    <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
-                      className="img-fluid" alt="Sample image" />
-
+                    <img
+                      src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
+                      className="img-fluid"
+                      alt="Sample image"
+                    />
                   </div>
                 </div>
               </div>
@@ -69,10 +115,8 @@ const SignupPage = () => {
           </div>
         </div>
       </div>
-    </section>
-      
     </div>
-  )
-}
+  );
+};
 
-export default SignupPage
+export default SignupPage;
