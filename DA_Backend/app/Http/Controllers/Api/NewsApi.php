fbf165f6f857eed922ecfee4b905af636b325c92
khongquantrong <105421649+khongquantrong@users.noreply.github.com>
2023-10-06 22:28:49 +0700

    api tin tức (thêm sửa xóa)

---
+++ DA_Backend/app/Http/Controllers/Api/NewsApi.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\News;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Log;
+class NewsApi extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        
+        try {
+            $data = News::query()->orderByDesc('id')->get();
+
+            return response()->json($data);
+        } catch (\Exception $exception) {
+            Log::error('Exception', [$exception]);
+
+            return response()->json(
+                ['err' => 'khong show dc tin tức'],
+                 Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+    
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        try {
+            $data =  new News();
+            $data->fill($request->all());
+            $data->save();
+            return response()->json($data);
+        } catch (\Exception $exception) {
+            Log::error('Exception', [$exception]);
+
+            return response()->json(
+                ['err' => 'khong them dc tin tức'],
+                 Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(News $new)
+    {
+        return response()->json($new);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+      
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        try {
+            $data =  News::findOrFail($id);
+            $data->fill($request->all());
+            $data->save();
+            return response()->json($data);
+        } catch (\Exception $exception) {
+            Log::error('Exception', [$exception]);
+            return response()->json(
+                ['err' => 'khong sua dc tin tức'],
+                 Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        
+        try {
+            $data =  News::destroy($id);
+            return response()->json($data);
+        } catch (\Exception $exception) {
+            Log::error('Exception', [$exception]);
+
+            return response()->json(
+                ['err' => 'khong xoa dc tin tức'],
+                 Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+}
