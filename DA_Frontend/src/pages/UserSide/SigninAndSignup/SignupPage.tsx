6b1da6a9d1e7304e6205c9a80408a685f3c5ca56
ducanh2k3 <105410311+ducanh2k3@users.noreply.github.com>
2023-10-10 21:32:38 +0700

    change_signup_2

--- DA_Frontend/src/pages/UserSide/SigninAndSignup/SignupPage.tsx
+++ DA_Frontend/src/pages/UserSide/SigninAndSignup/SignupPage.tsx
@@ -3,34 +3,36 @@ import { Button, Checkbox, Form, Input } from "antd";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { IUser } from "../../../interface/user";
-import { Alert, Space } from 'antd';
+import { useState } from "react";
+import { Alert, Space } from "antd";
+import {  notification } from "antd";
 const SignupPage = (props) => {
+  const [api, contextHolder] = notification.useNotification();
   const navigate = useNavigate();
   type FieldType = {
     name?: string;
     password?: string;
-    // password2?: string;
     role_id?: string;
     email?: string;
     remember?: string;
   };
-
-  const onClose = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    console.log(e, 'I was closed.');
+  const openNotification = () => {
+    api.open({
+      message: "Notification Title",
+      description: props.mess,
+      duration: 3,
+      style: {
+        backgroundColor: "red", // Set the background color to red
+      },
+    });
   };
-  console.log(props)
   const onFinish = (values: FieldType) => {
-    // Call the onAddUsers function with the user data
-    props.onAddUsers(values).then(()=>{
-      navigate("/")
-    })
-    console.log(values)
-    console.log(props.mess)
-    
-  
+   
+    props.onAddUsers(values);
+    console.log(props.mess);
 
-    // Redirect to the desired location after signup
-    // navigate('/dashboard'); // You can change '/dashboard' to your desired route
+    {props.mess && openNotification}
+
   };
 
   const onFinishFailed = (errorInfo: any) => {
@@ -39,17 +41,9 @@ const SignupPage = (props) => {
 
   return (
     <div>
-   { props.mess!=undefined &&   <Space direction="vertical" style={{ width: '100%' }}>
-   <Alert
-      message="Error Text"
-      description={ props.mess}
-      type="error"
-      closable
-      onClose={onClose}
-    />
-  </Space>}
       <div className="container  h-100; vh-100">
         <div className="row d-flex justify-content-center align-items-center h-100">
+          {contextHolder}
           <div className="col-lg-12 col-xl-11">
             <div className="card text-black" style={{ borderRadius: "25px" }}>
               <div className="card-body p-md-4">
@@ -70,7 +64,6 @@ const SignupPage = (props) => {
                         onFinishFailed={onFinishFailed}
                         autoComplete="off"
                       >
-
                         <Form.Item<FieldType>
                           label=""
                           name="role_id"
@@ -81,7 +74,7 @@ const SignupPage = (props) => {
                             },
                           ]}
                           initialValue="3"
-                          style={{ display: 'none' }}
+                          style={{ display: "none" }}
                         >
                           <Input />
                         </Form.Item>
@@ -101,7 +94,16 @@ const SignupPage = (props) => {
                         <Form.Item
                           label="Phone"
                           name="phone"
-                          rules={[{ required: true, message: "Vui lòng nhập số điện thoại" }]}
+                          rules={[
+                            {
+                              required: true,
+                              message: "Vui lòng nhập số điện thoại",
+                            },
+                            {
+                              pattern: /^[0-9]{10}$/, // Regular expression to validate a 10-digit phone number
+                              message: "Số điện thoại không hợp lệ",
+                            },
+                          ]}
                         >
                           <Input />
                         </Form.Item>
@@ -111,14 +113,14 @@ const SignupPage = (props) => {
                           rules={[
                             {
                               required: true,
-                              pattern: /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/,
+                              pattern:
+                                /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/,
                               message: "Email không hợp lệ",
                             },
                           ]}
                         >
                           <Input />
                         </Form.Item>
-
                         <Form.Item<FieldType>
                           label="Mật khẩu"
                           name="password"
@@ -126,33 +128,55 @@ const SignupPage = (props) => {
                             {
                               required: true,
                               message: "Vui lòng nhập mật khẩu",
+                            },
+                            {
                               min: 6,
+                              message: "Mật khẩu phải có ít nhất 6 ký tự",
                             },
                           ]}
                         >
                           <Input.Password />
                         </Form.Item>
-                        <Form.Item<FieldType>
+                        <Form.Item
                           label="Nhập lại mật khẩu"
                           name="password2"
+                          dependencies={["password"]}
+                          hasFeedback
                           rules={[
                             {
                               required: true,
-                              message: "Vui lòng nhập mật khẩu",
-                              min: 6,
+                              message: "Vui lòng nhập lại mật khẩu",
                             },
+                            ({ getFieldValue }) => ({
+                              validator(_, value) {
+                                if (
+                                  !value ||
+                                  getFieldValue("password") === value
+                                ) {
+                                  return Promise.resolve();
+                                }
+                                return Promise.reject(
+                                  new Error("Mật khẩu không khớp")
+                                );
+                              },
+                            }),
                           ]}
                         >
                           <Input.Password />
                         </Form.Item>
 
                         <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-                          <Button type="primary" htmlType="submit">
+                          <Button type="primary" htmlType="submit" >
                             Đăng ký
                           </Button>
                         </Form.Item>
                       </Form>
-                      <span style={{ paddingLeft: '100px' }}>Bạn đã có tài khoản? <Link style={{ textDecoration: 'none' }} to="/signin" >Đăng nhập</Link></span>
+                      <span style={{ paddingLeft: "100px" }}>
+                        Bạn đã có tài khoản?{" "}
+                        <Link style={{ textDecoration: "none" }} to="/signin">
+                          Đăng nhập
+                        </Link>
+                      </span>
                     </div>
                   </div>
                   <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
