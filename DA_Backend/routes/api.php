5a6a99774a53c440c13738a8d52c53a431e7b891
Dinh Viet Duc <dinhvietduc.isme@gmail.com>
2023-10-11 15:04:02 +0700

    Update route

--- DA_Backend/routes/api.php
+++ DA_Backend/routes/api.php
@@ -29,51 +29,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('register', [RegisterApi::class, 'register']);
 Route::post('login', [LoginApi::class, 'login']);
 Route::post('logout', [LoginApi::class, 'logout']);
-Route::resource('news',NewsApi::class);
-Route::prefix('news')->group(function () {
-    Route::get('/', [ClientNewsApi::class, 'index']);
-    Route::get('/{id}', [ClientNewsApi::class, 'show']);
-});
-
-Route::resource('users',UserApi::class);
-
-//api staff 
-Route::prefix('staff')->group(function () {
-    Route::get('/', [StaffApi::class, 'index']);
-    Route::post('/', [StaffApi::class, 'store']);
-    Route::get('/{id}', [StaffApi::class, 'show']);
-    Route::put('/{id}', [StaffApi::class, 'update']);
-    Route::delete('/{id}', [StaffApi::class, 'destroy']);
-});
-//api items
-Route::prefix('item')->group(function () {
-    Route::get('/', [ItemApi::class, 'index']);
-    Route::post('/', [ItemApi::class, 'store']);
-    Route::get('/{id}', [ItemApi::class, 'show']);
-    Route::put('/{id}', [ItemApi::class, 'update']);
-    Route::delete('/{id}', [ItemApi::class, 'destroy']);
-});
-
-//api role
-Route::prefix('role')->group(function () {
-    Route::get('/', [RoleApi::class, 'index']);
-    Route::post('/', [RoleApi::class, 'store']);
-    Route::get('/{id}', [RoleApi::class, 'show']);
-    Route::put('/{id}', [RoleApi::class, 'update']);
-    Route::delete('/{id}', [RoleApi::class, 'destroy']);
-});
-//api review
-Route::prefix('review')->group(function () {
-    Route::get('/', [ReviewApi::class, 'index']);
-    Route::post('/', [ReviewApi::class, 'store']);
-    Route::get('/{id}', [ReviewApi::class, 'show']);
-    Route::put('/{id}', [ReviewApi::class, 'update']);
-    Route::delete('/{id}', [ReviewApi::class, 'destroy']);
-    // lấy review qua người dùng
-    Route::get('user/{userId}', [ReviewApi::class, 'showByUser']);
-    // lấy review qua dịch vụ
-    Route::get('service/{serviceId}', [ReviewApi::class, 'showByService']);
-});
 
  //api admin
 Route::prefix('admin')->group(function () {
