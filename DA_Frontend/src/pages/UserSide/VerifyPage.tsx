9a8e81caa157d3d7e16b82f2802771d15169e2ff
ducanh2k3 <105410311+ducanh2k3@users.noreply.github.com>
2023-10-14 17:18:59 +0700

    verify

---
+++ DA_Frontend/src/pages/UserSide/VerifyPage.tsx
@@ -0,0 +1,139 @@
+import React from "react";
+import { Button, Checkbox, Form, Input } from "antd";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { IUser } from "../../../interface/user";
+import { useState } from "react";
+import { Alert, Space } from "antd";
+import { notification } from "antd";
+import instance from "../../api/instance";
+import { useParams } from "react-router-dom";
+const VerifyPage = () => {
+  // Use the useParams hook to get the `sdt` parameter from the URL
+  const { sdt } = useParams();
+  const navigate = useNavigate();
+  const [api, contextHolder] = notification.useNotification();
+  const [showNotification, setShowNotification] = useState(false);
+  const verify = (values) => {
+    
+    return instance
+      .post("register/verify-code", values)
+      .then((response) => {
+        // Handle the response as needed
+        if(response.data == 'Xác minh mã thành công') {
+          navigate("/")
+        }
+      })
+      .catch((error) => {
+        // Handle errors
+        console.error("Error:", error);
+        throw error;
+      });
+  };
+  const openNotification = (mess, text_color, bg_color, title) => {
+    setShowNotification(true);
+    api.open({
+      message: title,
+      description: mess,
+      duration: 3,
+      style: {
+        backgroundColor: bg_color,
+        color: text_color,
+      },
+      onClose: () => {
+        setShowNotification(false);
+      },
+    });
+  };
+  const onFinish = (values) => {
+    verify(values)
+      .then((response) => {
+        if (response == "Đăng kí thành công") {
+          openNotification(response, "black", "green", "Success");
+        } else if (
+          response == "Số điện thoại đã tồn tại" ||
+          response == "Email đã tồn tại"
+        ) {
+          return openNotification(response, "white", "red", "Failed");
+        }
+      })
+      .then(() => {
+        console.log(sdt, values);
+      })
+      .catch((error) => {
+        // Handle any errors here if needed
+        console.error("Error:", error);
+        throw error; // Rethrow the error for further handling in your component
+      });
+  };
+  const onFinishFailed = (errorInfo: any) => {
+    console.log("Failed:", errorInfo);
+  };
+  return (
+    <div>
+      <div className="container  h-100; vh-100">
+        <div className="row d-flex justify-content-center align-items-center h-100">
+          {contextHolder}
+          <div className="col-lg-12 col-xl-11">
+            <div className="card text-black" style={{ borderRadius: "25px" }}>
+              <div className="card-body p-md-4">
+                <div className="row justify-content-center">
+                  <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+                    <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">
+                      Nhập mã xác thực
+                    </p>
+                    <p> Mã xác thực của bạn đã được gửi về số +84 {sdt}</p>
+                    <div>
+                      <Form
+                        name="basic"
+                        labelCol={{ span: 8 }}
+                        wrapperCol={{ span: 16 }}
+                        style={{ maxWidth: 600 }}
+                        initialValues={{ remember: true }}
+                        onFinish={onFinish}
+                        onFinishFailed={onFinishFailed}
+                        autoComplete="off"
+                      >
+                        <Form.Item
+                          label="Code"
+                          name="code"
+                          rules={[
+                            {
+                              required: true,
+                              message:
+                                "Vui lòng nhập mã số được gửi vào  điện thoại bạn ",
+                            },
+                          ]}
+                        >
+                          <Input />
+                        </Form.Item>
+                        <Form.Item
+                          name="phone"
+                          initialValue={sdt} // Set the initial value to {sdt}
+                        ></Form.Item>
+                        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                          <Button type="primary" htmlType="submit">
+                            Xác thực
+                          </Button>
+                        </Form.Item>
+                      </Form>
+                    </div>
+                  </div>
+                  <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+                    <img
+                      src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
+                      className="img-fluid"
+                      alt="Sample image"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default VerifyPage;
