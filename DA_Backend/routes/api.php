92d6666553c7ede01c3f50b6aae43fd13bdf9318
Dinh Viet Duc <dinhvietduc.isme@gmail.com>
2023-10-15 13:53:08 +0700

    fix route

--- DA_Backend/routes/api.php
+++ DA_Backend/routes/api.php
@@ -2,14 +2,15 @@
 
 use App\Http\Controllers\API\Admin\NewsApi;
 use App\Http\Controllers\Api\Admin\ServiceApi;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\Admin\UserApi;
 use App\Http\Controllers\Api\Auth\LoginApi;
 use App\Http\Controllers\Api\Auth\RegisterApi;
 use App\Http\Controllers\Api\Client\NewsApi as ClientNewsApi;
 use App\Http\Controllers\Api\Admin\ServiceItemApi;
 use App\Http\Controllers\Api\Admin\ServiceLevelApi;
+use App\Http\Controllers\API\Admin\ReviewApi;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,48 +27,54 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-//api dang nhap dang ky
+// Authentication and Registration APIs
 Route::post('register', [RegisterApi::class, 'register']);
 Route::post('login', [LoginApi::class, 'login']);
 Route::post('logout', [LoginApi::class, 'logout']);
 Route::post('register/resend-verification-code', [RegisterApi::class, 'resendVerificationCode']);
 Route::post('register/verify-code', [RegisterApi::class,'verifyCode']);
- //api admin
-Route::prefix('admin')->group(function () {
-//api review
-Route::prefix('review')->group(function () {
-    Route::get('/', [ReviewApi::class, 'index']);
-    Route::post('/', [ReviewApi::class, 'store']);
-    Route::get('/{id}', [ReviewApi::class, 'show']);
-    Route::put('/{id}', [ReviewApi::class, 'update']);
-    Route::delete('/{id}', [ReviewApi::class, 'destroy']);
-});
-//api user 
-Route::resource('users', [UserApi::class]);
-    Route::resource('users',UserApi::class);
-    //api quan ly tin tuc  http://127.0.0.1:8000/api/admin/news
-    Route::resource('news',NewsApi::class);
-    //api quan ly service  http://127.0.0.1:8000/api/admin/services
-    Route::resource('services',ServiceApi::class);
-    //api quan ly service item   http://127.0.0.1:8000/api/admin/service_item
-    Route::resource('service_item',ServiceItemApi::class);
-    //api quan ly service level  http://127.0.0.1:8000/api/admin/service_level
-    Route::resource('service_level',ServiceLevelApi::class);
-});
- 
 
- //api client
-Route::prefix('client')->group(function () {   
-    //api tin tuc 
+// Admin APIs
+Route::prefix('admin')->group(function () {
+    // Review APIs
+    Route::prefix('review')->group(function () {
+        Route::get('/', [ReviewApi::class, 'index']);
+        Route::post('/', [ReviewApi::class, 'store']);
+        Route::get('/{id}', [ReviewApi::class, 'show']);
+        Route::put('/{id}', [ReviewApi::class, 'update']);
+        Route::delete('/{id}', [ReviewApi::class, 'destroy']);
+    });
+
+    // User API
+    Route::resource('users', UserApi::class);
+
+    // News API
+    Route::resource('news', NewsApi::class);
+
+    // Service API
+    Route::resource('services', ServiceApi::class);
+
+    // Service Item API
+    Route::resource('service_item', ServiceItemApi::class);
+
+    // Service Level API
+    Route::resource('service_level', ServiceLevelApi::class);
+});
+
+// Client APIs
+Route::prefix('client')->group(function () {
+    // News API
     Route::prefix('news')->group(function () {
         Route::get('/', [ClientNewsApi::class, 'index']);
         Route::get('/{id}', [ClientNewsApi::class, 'show']);
     });
-    //api review
+
+    // Review APIs
     Route::prefix('review')->group(function () {
-        // lấy review qua người dùng
+        // Get reviews by user
         Route::get('user/{userId}', [ReviewApi::class, 'showByUser']);
-        // lấy review qua dịch vụ
+
+        // Get reviews by service
         Route::get('service/{serviceId}', [ReviewApi::class, 'showByService']);
     });
 });
