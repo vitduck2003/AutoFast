e8dceadaaeddf840b2053345ee49bfcef48f11c1
ducanh2k3 <105410311+ducanh2k3@users.noreply.github.com>
2023-10-18 22:05:44 +0700

    fixform

--- DA_Frontend/src/api/instance.tsx
+++ DA_Frontend/src/api/instance.tsx
@@ -2,8 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
     baseURL: 'http://localhost:8000/api'
-    // http://localhost:3000
-    // http://localhost:8000/api
+
 })
 
 export default instance;
