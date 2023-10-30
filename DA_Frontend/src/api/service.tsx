64a2497d4a223f516155fac7c6196cdb54b40235
Phong Pham <ducphong2713@gmail.com>
2023-10-30 17:14:25 +0700

    API Service and Service Item

--- DA_Frontend/src/api/service.tsx
+++ DA_Frontend/src/api/service.tsx
@@ -4,26 +4,26 @@ const getService = () => {
   return instance.get("/admin/services");
 };
 const addService = (service: IService) => {
-  return instance.post("/service", service);
+  return instance.post("/admin/service", service);
 };
 const updateService = (service: IService) => {
-  return instance.patch("/service" + service.id, service);
+  return instance.patch("/admin/service" + service.id, service);
 };
 const deleteService= (id: any) => {
-  return instance.delete(`/service/${id}`);
+  return instance.delete(`/admin/service/${id}`);
 };
 
 const getServiceItem = () => {
   return instance.get("/client/service-item");
 };
 const addServiceItem = (service: ISeviceItem) => {
-  return instance.post("/serviceitem", service);
+  return instance.post("/client/serviceitem", service);
 };
 const updateServiceItem = (service: ISeviceItem) => {
-  return instance.patch(`/serviceitem/${service.id}/` , service);
+  return instance.patch(`/client/serviceitem/${service.id}/` , service);
 };
 const deleteServiceItem= (id: any) => {
-  return instance.delete(`/serviceitem${id}` );
+  return instance.delete(`/client/serviceitem${id}` );
 };
 
 
