1955baddfb3249dfd0767cc2372f482ac37e08ec
ducanh2k3 <105410311+ducanh2k3@users.noreply.github.com>
2023-11-05 21:07:36 +0700

    2

--- DA_Frontend/src/pages/UserSide/Booking/MyBooking.tsx
+++ DA_Frontend/src/pages/UserSide/Booking/MyBooking.tsx
@@ -4,6 +4,7 @@ import instance from "../../../api/instance";
 const MyBooking = () => {
   const [phone, setPhone] = useState("");
   const [bookings, setBookings] = useState([]);
+  const [selectedJobDetails, setSelectedJobDetails] = useState(null);
 
   useEffect(() => {
     const storedUser = sessionStorage.getItem("user");
@@ -42,11 +43,11 @@ const MyBooking = () => {
     textAlign: 'center', // centers the text inside the container
   };
   
-  const tableStyle = {
-    width: '80%', // sets the width of the table to 80% of its container
-    borderCollapse: 'collapse',
-    marginTop: '20px',
-  };
+  // const tableStyle = {
+  //   width: '80%', // sets the width of the table to 80% of its container
+  //   borderCollapse: 'collapse',
+  //   marginTop: '20px',
+  // };
   
   const thStyle = {
     backgroundColor: '#f0f0f0',
@@ -70,8 +71,68 @@ const MyBooking = () => {
   const goToPayment = (bookingId) => {
     history.push(`/payment/${bookingId}`); // Adjust the path as needed
   };
+  const showJobDetails = (jobs) => {
+    setSelectedJobDetails(jobs);
+  };
 
+  // Function to close job details view
+  const closeJobDetails = () => {
+    setSelectedJobDetails(null);
+  };
+  const backdropStyle = {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    zIndex: 1050, // Make sure it's on top of other content
+  };
+  
+  // Styles for the modal content area
+  const modalContentStyle = {
+    backgroundColor: '#ffffff',
+    padding: '20px',
+    borderRadius: '8px',
+    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
+    width: '90%', // You can adjust the width as needed
+    maxWidth: '600px', // You can adjust the maximum width as needed
+    maxHeight: '90vh',
+    overflowY: 'auto',
+  };
 
+  const tableStyle = {
+    margin: "0 auto",
+    textAlign: "center",
+  };
+  
+  // Styles for the modal header, if you have one
+  const modalHeaderStyle = {
+    borderBottom: '1px solid #eeeeee',
+    paddingBottom: '10px',
+    marginBottom: '20px',
+  };
+  
+  // Styles for the modal body
+  const modalBodyStyle = {
+    paddingBottom: '20px',
+    marginBottom: '20px',
+  };
+  
+  // Styles for the close button
+  const closeButtonStyle = {
+    backgroundColor: '#007bff', // Bootstrap primary color
+    color: 'white',
+    border: 'none',
+    padding: '10px 20px',
+    borderRadius: '5px',
+    cursor: 'pointer',
+    float: 'right', // If you want it to be on the right
+  };
+  
   const liStyle = {
     padding: '3px',
   };
@@ -83,6 +144,60 @@ const MyBooking = () => {
   return (
     <div style={containerStyle}>
       <h1 style={{ textAlign: 'center' }}>Lịch của tôi</h1>
+      {/* {selectedJobDetails && (
+            <div style={{ padding: '20px', border: '1px solid #ccc', marginTop: '20px', borderRadius: '5px', position: 'absolute', background: 'white', zIndex: '10' }}>
+              <h2>Các công việc</h2>
+              <ul style={ulStyle}>
+                {selectedJobDetails.map((job) => (
+                  <li key={job.id} style={liStyle}>
+                    {job.item_name} - {job.item_price}
+                  </li>
+                ))}
+              </ul>
+              <button style={buttonStyle} onClick={closeJobDetails}>
+                Đóng
+              </button>
+            </div>
+          )} */}
+          {selectedJobDetails && (
+    <div style={backdropStyle}>
+      <div style={modalContentStyle}>
+        <div style={modalHeaderStyle}>
+          <h4>Chi tiết công việc</h4>
+        </div>
+        <div style={modalBodyStyle}>
+          <table style={tableStyle}>
+            <thead>
+              <tr>
+                <th style={thStyle}>Tên công việc</th>
+                <th style={thStyle}>Thời gian dự kiến hoàn thành</th>
+                <th style={thStyle}>Trạng Thái </th>
+                <th style={thStyle}>Tên người phụ trách </th>
+
+                <th style={thStyle}>Giá tiền</th>
+              </tr>
+            </thead>
+            <tbody>
+              {selectedJobDetails.map((job) => (
+                <tr key={job.id}>
+                  <td style={tdStyle}>{job.item_name}</td>
+                  <td style={tdStyle}>{job.target_time_done}</td>
+                  <td style={tdStyle}>{job.status}</td>
+                
+                  <td style={tdStyle}>{job.staff_name}</td>
+
+                  <td style={tdStyle}>{job.item_price.toLocaleString()} VND</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <button style={closeButtonStyle} onClick={closeJobDetails}>
+          Đóng
+        </button>
+      </div>
+    </div>
+  )}
       {bookings.length > 0 ? (
         <table style={tableStyle}>
           <thead>
@@ -121,12 +236,12 @@ const MyBooking = () => {
                   </ul>
                 </td>
               
-                {/* ... other table cells ... */}
+              
                 <td style={tdStyle}>
                  
-                  <button  onClick={() => {/* Define function to show job details */}}>
-                    Xem chi tiết
-                  </button>
+                <button style={buttonStyle} onClick={() => showJobDetails(booking.jobs)}>
+                      Xem chi tiết
+                    </button>
 
                
                   {booking.booking.status === 'Đã hoàn thành' && (
@@ -140,6 +255,7 @@ const MyBooking = () => {
             ))}
           </tbody>
         </table>
+        
       ) : (
         <p>No bookings found.</p>
       )}
