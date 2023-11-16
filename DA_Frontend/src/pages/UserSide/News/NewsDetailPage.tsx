6ab886db4e1a38f5d7252481dd7a49e252648d4b
sondev03 <123224711+sondev03@users.noreply.github.com>
2023-11-17 00:06:10 +0700

    new detail

--- DA_Frontend/src/pages/UserSide/News/NewsDetailPage.tsx
+++ DA_Frontend/src/pages/UserSide/News/NewsDetailPage.tsx
@@ -9,19 +9,110 @@ const NewsDetailPage = (props: any) => {
   useEffect(() => {
     setNews(props.news.find(New => New.id == id))
 })
+const containerStyle = {
+  maxWidth: '800px',
+  margin: '0 auto',
+  padding: '20px',
+};
 
-  return (
-    <div>
-      
-      <div>
-      <p>{New?.title}</p>
-           <p>{New?.content}</p>
-           <p>{New?.date}</p>
+const headerStyle = {
+  textAlign: 'center',
+  backgroundColor: '#333',
+  color: '#fff',
+  padding: '10px',
+};
+const headerStyle2 = {
+  textAlign: 'center',
+  backgroundColor: '#333',
+  color: '#fff',
+  padding: '10px',
+};
+const headerStyle3 = {
+  textAlign: 'center',
+  backgroundColor: '#333',
+  color: '#fff',
+  padding: '10px',
+};
+const headerStyle4 = {
+  textAlign: 'center',
+  backgroundColor: '#333',
+  color: '#fff',
+  padding: '10px',
+};
+const headerStyle5 = {
+  textAlign: 'center',
+  backgroundColor: '#333',
+  color: '#fff',
+  padding: '10px',
+};
+const headerStyle6 = {
+  textAlign: 'center',
+  backgroundColor: '#333',
+  color: '#fff',
+  padding: '10px',
+};
+
+const headerStyle7 = {
+  textAlign: 'center',
+  backgroundColor: '#333',
+  color: '#fff',
+  padding: '10px',
+};
+const headerStyle8 = {
+  textAlign: 'center',
+  backgroundColor: '#333',
+  color: '#fff',
+  padding: '10px',
+};
+const headerStyle9 = {
+  textAlign: 'center',
+  backgroundColor: '#333',
+  color: '#fff',
+  padding: '10px',
+};
+
+const articleStyle = {
+  backgroundColor: '#fff',
+  padding: '20px',
+  marginTop: '20px',
+  borderRadius: '8px',
+  boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
+};
+
+const imageStyle = {
+  maxWidth: '100%',
+  height: 'auto',
+  borderRadius: '8px',
+  marginBottom: '15px',
+};
+
+const dateAuthorStyle = {
+  color: '#888',
+  fontSize: '14px',
+};
+
+const contentStyle = {
+  lineHeight: '1.6',
+};
+    return (
+      <div style={containerStyle}>
+          <header >
+              <h1>{New?.title}</h1>
+          </header>
+    
+          <article style={articleStyle}>
+              <img src="your-image-url.jpg" alt="Hình ảnh tin tức" style={imageStyle} />
+              <p style={dateAuthorStyle}>Ngày Đăng: 16 tháng 11, 2023</p>
+              <p style={dateAuthorStyle}>{New?.des}</p>
+              <p style={contentStyle}>
+                 {New?.content}
+              </p>
+          </article>
       </div>
-           
-            
-        </div>
-  )
+    );
+  
 }
 
 export default NewsDetailPage
+
+
