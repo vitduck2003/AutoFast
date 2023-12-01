c9a32bda1aa9a7466b81a75d0aa1ea710a8a0ce8
sondev03 <123224711+sondev03@users.noreply.github.com>
2023-12-01 22:14:57 +0700

    review_new

--- DA_Frontend/src/pages/UserSide/Review/Review.tsx
+++ DA_Frontend/src/pages/UserSide/Review/Review.tsx
@@ -4,15 +4,38 @@ import { useParams } from 'react-router';
 import { Rate } from 'antd';
 
 const Review = ({bookingdetail, service}) => {
-  console.log(bookingdetail);
-  console.log(service);
+  // console.log(bookingdetail);
+  // console.log(service);
   
   
   const { id } = useParams()
-  console.log(id);
+  // console.log(id);
   
   const [review, setNews] = useState<any>([]);
-  console.log(review);
+  // console.log(review);
+  
+  const [filteredDetails, setFilteredDetails] = useState([]);
+  useEffect(() => {
+    const filtered = bookingdetail.filter(detail => detail.id_booking == id);
+    setFilteredDetails(filtered);
+  }, [bookingdetail, id]);
+
+  console.log(filteredDetails);
+ 
+  
+  const idService = filteredDetails.length > 0 ? filteredDetails[0].id_service : null;
+  console.log(idService);
+
+  const [filteredService, setFilteredService] = useState([]);
+  useEffect(() => {
+    const filtered = service.filter(service => service.id == idService);
+    setFilteredService(filtered);
+  }, [service, idService]);
+
+  // Lấy service_name
+  const serviceName = filteredService.length > 0 ? filteredService[0].service_name : '';
+
+
   
 //   useEffect(() => {
 //     setNews(props.booking.find((review: { id: string | undefined; }) => review.id == id))
@@ -20,12 +43,12 @@ const Review = ({bookingdetail, service}) => {
   return (
     <div><div className="container mt-5">
     <h2 className="text-center mb-4">Đánh Giá Sản Phẩm</h2>
-  
+    
    
     <form>
       <div className="form-group">
         <label htmlFor="productName">Tên Dịch Vụ</label>
-        <input type="text" className="form-control" id="productName" placeholder="Nhập tên sản phẩm" />
+        <input type="text" className="form-control" id="productName" value={serviceName} disabled placeholder="Nhập tên sản phẩm" />
       </div>
       <div className="form-group">
         <label htmlFor="review">Đánh Giá:</label>
